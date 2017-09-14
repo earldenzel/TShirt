5cb7f2a0b0b4c4ecf74460d9cbaa2365192f3b5d
earldenzel <earldenzel@gmail.com>
2017-09-14 00:10:17 -0400

    Initial Commit
    [ADD] Lab 2 Scene, 1 player sprite, 3 shirt sprites, 3 button sprites,
    working buttons

---
+++ Assets/Scripts/ShirtChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShirtChanger : MonoBehaviour {
+
+    public GameObject shirt;
+    public Sprite shirtChange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        shirt.GetComponent<SpriteRenderer>().sprite = shirtChange;
+    }
+}
